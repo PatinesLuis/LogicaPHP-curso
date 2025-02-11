b1f91772825b49f71e9135ea127a8f87847bcb32
PatinesLuis <luispatines17@gmail.com>
2025-02-11 19:19:48 -0300

    seção arrays avançados

---
+++ ArraysAvançados/Adicionandovalores.php
@@ -0,0 +1,21 @@
+<?php
+
+$array = [];
+
+print_r($array);
+echo "<br>";
+
+$array[0] = "teste";
+print_r($array);
+echo "<br>";
+
+$array[1] = "irineu";
+print_r($array);
+echo "<br>";
+
+//adicionar um valor ao fim do array
+
+$array[] = "Adicionando ao fim";
+print_r($array);
+echo "<br>";
+
---
+++ ArraysAvançados/criarArrayRapido.php
@@ -0,0 +1,9 @@
+<?php
+
+$arr = range(1,10);
+
+print_r($arr);
+echo "<br>";
+
+$array_pulando = range(1,10,2);
+print_r($array_pulando);
